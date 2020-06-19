c961cfe7d78323a6b0ce1a6079207074c12bdf50
AditiKapoor56 <a502@my.centennialcollege.ca>
2020-06-19 18:37:08 -0400

    Add project files.

---
+++ COMP123_M2020_LAB04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_LAB04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person param = new Person("Aditi", age: 19);
+
+            aditi.SaysHello();
+
+            Console.ReadLine();
+        }
+    }
+}
